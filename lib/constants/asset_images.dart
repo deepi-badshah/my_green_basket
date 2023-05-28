c00cb2848d61af6ec9d62016166581813c898976
Deepanshu <b420021@iiit-bh.ac.in>
2023-05-28 11:21:04 +0530

    Initial commit

---
+++ lib/constants/asset_images.dart
@@ -0,0 +1,8 @@
+class AssetsImages {
+  static AssetsImages instance = AssetsImages();
+
+  static const String _imagesPath = "assets/images";
+
+  final String welcomeImage = "$_imagesPath/fruits_basket1.jpeg";
+  final String googleLogo = "$_imagesPath/googlelogo.png";
+}
